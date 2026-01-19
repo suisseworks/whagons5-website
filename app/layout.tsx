d26b11052cf95af60ec5290a72bdf609c2f604db
Matthias <matthiasmalek72@gmail.com>
2026-01-19 17:15:13 -0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Whagons',
   description: 'Workflow management, simplified.',
+  icons: {
+    icon: '/whagons.svg',
+  },
 };
 
 export default function RootLayout({
